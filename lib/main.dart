0652a55b382190635b660a646803815691df7139
qyinm <qusseun@gmail.com>
2026-01-03 01:00:16 +0900

    feat: Firebase 및 Google 로그인 설정 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:glimm/core/di/injection.dart';
 import 'package:glimm/core/theme/glimm_theme.dart';
 import 'package:glimm/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:glimm/features/auth/presentation/pages/login_page.dart';
-
-// TODO: Add firebase_options.dart when generated
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await configureDependencies();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  // For MVP/Dev without generated options, we might skip or assume pre-setup
-  // But usually we need `flutterfire configure`.
-  // I will comment it out until user runs setup.
+  await configureDependencies();
 
   runApp(const GlimmApp());
 }
